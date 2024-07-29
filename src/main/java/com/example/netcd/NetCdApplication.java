bb923e97084ab4b1256b4bac088555e4c209269b
Sprite137 <3304424497@qq.com>
2024-07-30 04:20:34 +0800

    first commit

---
+++ src/main/java/com/example/netcd/NetCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.netcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netcd/NetCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
